ad7d7fe712a9156329686f53a4cb90890805cdbc
Anbuselvan <sanbu0407@gmail.com>
2025-09-05 13:35:46 +0530

    last

--- config/db.js
+++ config/db.js
@@ -2,21 +2,16 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("❌ Aiven DB connection failed:", err);
-  } else {
-    console.log("✅ Connected to Aiven Database");
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env.DB_CA_CERT  // CA cert will come from Render env var
   }
 });
 
-export default db;
+export default pool;
